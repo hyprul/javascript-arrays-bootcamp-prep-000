b06ab806b583d64288e2bcfc3812264715843ef6
Sean Huang <hyprul@ucla.edu>
2018-04-30 00:45:45 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,17 +1,19 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"];
 
-function addElementToBeginningOfArray(){
-  
+function addElementToBeginningOfArray(array, element){
+  var morearray = [element, ...array]
+  return morearray
 }
 
-function destrutivelyAddElementToBeginningOfArray(){
-  
+function destrutivelyAddElementToBeginningOfArray(array, elemnent){
+  array = [element, ...array]
+  return array
 }
 
-function addElementToEndOfArray(){
-  
+function addElementToEndOfArray(array, element){
+  var morearray =[...array, element]
 }
 
-function destructivelyAddElementToEndOfArray(){
+function destructivelyAddElementToEndOfArray(array, element){
   
 }
